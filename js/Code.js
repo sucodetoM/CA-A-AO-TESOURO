8a3a8f159cdaa5014fcb85404630ed260192a690
sucodetoM <137896231+sucodetoM@users.noreply.github.com>
2023-09-26 09:18:56 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "ONE PIECE";
+const accessCode2 = "LIVE ACTION";
+const accessCode3 = "SUNNY E GOINGMERRY";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("P C I E E O E N", 100,50)
+    fill("lightblue")
+    text("Dica: um anime de um pirata!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("I L A C O N V E T I", 700,150)
+    fill("lightblue")
+    text("Dica: O ANIME DA PERGUNTA ACIMA GANHOU!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("G O I N G S U N Y N M R R Y E E", 100,250)
+    fill("lightblue")
+    text("Dica: OS DOIS BARCOS MAIS AMADOS DO ANIME(DOS PROTAGONISTAS)!", 100,270)
+
+}
